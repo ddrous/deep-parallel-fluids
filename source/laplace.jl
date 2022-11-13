bc6abcd233e3c598f76340ed52efd5274cb0120e
RDN WIN11 <desmond.ngueguin@gmail.com>
2022-11-13 20:03:02 +0000

    Finished Laplace problem

--- source/laplace.jl
+++ source/laplace.jl
@@ -1,5 +1,5 @@
-
 using Plots
+
 
 ## A simple rectangle regularly discretised
 mutable struct Geometry
@@ -9,24 +9,28 @@ mutable struct Geometry
     Ny::Int64
 end
 
-# exit()
 
+## Reshapes a vector into a Julia-ready matrix for computation
+vectomat(U::Vector{T}, N::Int64, M::Int64) where {T<:Number} = reshape(U, (N,M))
+
+
+## Flattens a matrix into a Julia-ready vector for computation
+mattovec(u::Matrix{T}) where {T<:Number} = vec(u)
+
+
+## Arranges a Julia-ready matrix into a 2D tensor suitable for human visualisation
+matto2d(u::Matrix{T}) where {T<:Number} = reverse(permutedims(u), dims=1)
+
+
+## A struct to hold out Laplace problem attributes
 mutable struct Laplace
     bc:: Float64
     u:: Matrix{Float64}
     f:: Matrix{Float64}
 end
 
-## Go from vector of size (N)*(M) to appropriate matrix 
-# vec2mat(U::Vector{T}, N::Int64, M::Int64) where {T<:Number} = reverse(permutedims(reshape(U, (M,N))), dims=1)
-vec2mat(U::Vector{T}, N::Int64, M::Int64) where {T<:Number} = reshape(U, (N,M))
 
-## Go from a matrix to a appropriate vector
-# mat2vec(u::Matrix{T}) where {T<:Number} = vec(permutedims(reverse(u, dims=1)))
-mat2vec(u::Matrix{T}) where {T<:Number} = vec(u)
-
-mat22d(u::Matrix{T}) where {T<:Number} = reverse(permutedims(u), dims=1)
-
+## Sets a boundary condition all around a matrix
 function setbc(u::Matrix{T}, bc::Float64, g::Geometry) where {T<:Number}
     u[1,:] .= bc
     u[g.Nx+1,:] .= bc
@@ -36,42 +40,43 @@ function setbc(u::Matrix{T}, bc::Float64, g::Geometry) where {T<:Number}
 end
 
 
+## Initiate a Laplace problem
+function initlaplace(g::Geometry; bc::Float64=1.0, fval::Float64=1.0)
+    u = zeros(Float64, (g.Nx+1,g.Ny+1))
+    setbc(u, bc, g)
+    f = fill(fval, (g.Nx+1,g.Ny+1))
+    return Laplace(bc, u, f)
+end
+
+
+## Deternimes location of a given point in a rectangle geometry
+## N.B. Assumes matrix form (Ny,Nx), not 2D form (Nx,Ny) obtained by rotation
 function isonboundary(i::Int64, j::Int64, g::Geometry):AbstractString
     if j==1 
-        return "south"
-    elseif i==g.Nx+1
         return "west"
+    elseif i==g.Nx+1
+        return "south"
     elseif j==g.Ny+1
-        return "north"
-    elseif i==1
         return "east"
+    elseif i==1
+        return "north"
     else
         return "inside"
     end
 end
 
 
-function initlaplace(g::Geometry)
-    bc = 0.5
-    u = zeros(Float64, (g.Nx+1,g.Ny+1))
-    setbc(u, bc, g)
-    f = ones(Float64, (g.Nx+1,g.Ny+1))
-    return Laplace(bc, u, f)
-end
-
-
+## Solve a Laplave problem
 function solvelaplace(l::Laplace, g::Geometry)
     n = (g.Nx-1)*(g.Ny-1)
-    U = zeros(Float64, n)
-    F = mat2vec(l.f[2:g.Nx, 2:g.Ny])
     A = zeros(Float64, (n,n))
+    F = mattovec(l.f[2:g.Nx, 2:g.Ny])
 
     Δx = g.Lx/g.Nx
     Δy = g.Ly/g.Ny
     val1 = 1.0/(Δx^2)
     val2 = 1.0/(Δy^2)
     val3 = 2.0 * (val1+val2)
-    println("Δx=", Δx)
 
     bcvals = Dict("south"=>val1, "east"=>val2, "north"=>val1, "west"=>val2)
 
@@ -84,9 +89,9 @@ function solvelaplace(l::Laplace, g::Geometry)
             b_name = isonboundary(ix, jy, g)
             if b_name == "inside"
                 if ix==i-1
-                    A[global_id-1, global_id] = -val1
+                    A[global_id, global_id-1] = -val1
                 elseif ix==i+1
-                    A[global_id+1, global_id] = -val1
+                    A[global_id, global_id+1] = -val1
                 elseif jy==j-1
                     A[global_id, global_id-g.Nx+1] = -val2
                 elseif jy==j+1
@@ -98,49 +103,42 @@ function solvelaplace(l::Laplace, g::Geometry)
         end
     end
 
-    ## Print stuff
-    println("A = \n", size(A))
-    println("F = \n", F)
-    show(stdout, "text/plain", vec2mat(F, g.Nx-1, g.Ny-1)')
+    ## Print constructed quantities
+    # show(stdout, "text/plain", A)
+    # show(stdout, "text/plain", F)
 
-    ## Solve system
+    ## Solve linear system
     U = A\F
-    l.u[2:g.Nx, 2:g.Ny] = vec2mat(U, g.Nx-1, g.Ny-1)
+    l.u[2:g.Nx, 2:g.Ny] = vectomat(U, g.Nx-1, g.Ny-1)
 
     return nothing
-
 end
 
+
+## visualize Laplace problem's solution
 function visualizelaplace(l::Laplace, g::Geometry)
+    ## Display solution in a 2D plane
+    println("\n----Solution----")
+    display(matto2d(l.u))
+    println()
+
+    ## Construct plot data: y axis first, then x axis (Julia convention)
     x=range(0,g.Lx,length=g.Nx+1)
     y=range(0,g.Ly,length=g.Ny+1)
-
-    # u = reverse(permutedims(l.u), dims=1)
-    # data = [u[i,j] for j in 1:g.Nx+1, i in 1:g.Ny+1]
-
     data = [l.u[i,j] for j in 1:g.Ny+1, i in 1:g.Nx+1]
 
-    ## Display solution in 2d plane
-    show(stdout, "text/plain", mat22d(l.u))
-
-    plot(x, y, data, st=:surface, camera=(60,30), xlabel = "x", ylabel = "y", zlabel = "u")
-
+    title = "Numerical solution of the Laplace problem"
+    plot(x, y, data, st=:surface, camera=(30,30), xlabel="x", ylabel="y", zlabel="u", title=title)
 end
 
 
-
-
-
-### Run the code
-
+### Run the code in a function: no global variables !
 function main()
-    geo = Geometry(1,1,8,5)
-    pb = initlaplace(geo)
-
+    geo = Geometry(1,1,100,50)
+    pb = initlaplace(geo; bc=1.0, fval=2.0)
     solvelaplace(pb, geo)
-    visualizelaplace(pb,geo)
+    visualizelaplace(pb, geo)
 end
+
 
 main()
-
-# exit()
